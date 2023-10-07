022e08aab20d59e719f3e756c44222362fb1986e
Gorkem Genc <gorkemgenc95@gmail.com>
2023-10-07 21:49:07 +0200

    create feature tests for News controller

---
+++ tests/Feature/NewsControllerTest.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\News;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class NewsControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    protected User $user;
+    protected News $news;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+    }
+
+    public function testListNews()
+    {
+        $this->generateNews(5);
+
+        $response = $this->get('/api/news');
+        $news = json_decode($response->getContent(), true);
+
+        $response->assertStatus(200);
+        $this->assertIsArray($news);
+        $this->assertCount(5, $news);
+        foreach ($news as $item) {
+            $this->assertArrayHasKey('title', $item);
+            $this->assertArrayHasKey('content', $item);
+        }
+    }
+
+    public function testListNewsWithPagination()
+    {
+        $this->generateNews(20);
+
+        $response = $this->get('/api/news?page=2');
+        $news = json_decode($response->getContent(), true);
+
+        $response->assertStatus(200);
+        $this->assertIsArray($news);
+        $this->assertCount(5, $news);
+        foreach ($news as $item) {
+            $this->assertArrayHasKey('title', $item);
+            $this->assertArrayHasKey('content', $item);
+        }
+    }
+
+    public function testShowNews()
+    {
+        $news = $this->generateNews(1);
+        $response = $this->get('/api/news/' . $news->id);
+        $newsData = json_decode($response->getContent(), true);
+
+        $response->assertStatus(200);
+        $this->assertEquals($news->title, $newsData['title']);
+        $this->assertEquals($news->content, $newsData['content']);
+    }
+
+    public function testStoreNews()
+    {
+        $data = [
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraph,
+        ];
+
+        $response = $this->postJson('/api/news', $data);
+
+        $response->assertStatus(201)
+        ->assertJson(['success' => true]);
+
+        $this->assertDatabaseHas('news', $data);
+    }
+
+    public function testUpdateNews()
+    {
+        $news = $this->generateNews(1);
+
+        $updatedData = [
+            'title' => 'Updated Title',
+            'content' => 'Updated Content',
+        ];
+
+        $response = $this->put('/api/news/' . $news->id, $updatedData);
+        $news->refresh();
+
+        $response->assertStatus(200);
+        $this->assertEquals($updatedData['title'], $news->title);
+        $this->assertEquals($updatedData['content'], $news->content);
+    }
+
+    public function testDeleteNews()
+    {
+        $news = $this->generateNews(1);
+        $response = $this->delete('/api/news/' . $news->id);
+        $deletedNews = News::query()->find($news->id);
+
+        $response->assertStatus(204);
+        $this->assertNull($deletedNews);
+    }
+
+    private function generateNews(int $count) {
+        $news = News::factory($count)->create(['user_id' => $this->user->id]);
+        return $count === 1 ? $news->first() : $news;
+    }
+
+}
