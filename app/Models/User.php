169039b4e7aa4e8066eb9a49467a782fcbe54285
Gorkem Genc <gorkemgenc95@gmail.com>
2023-10-07 23:43:25 +0200

    add relationship between user and news

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -11,6 +11,11 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    public function news(): HasMany
+    {
+        return $this->hasMany(News::class);
+    }
 
     /**
      * The attributes that are mass assignable.
